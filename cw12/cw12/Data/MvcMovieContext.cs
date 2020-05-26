dfb32c95e53a728bb6522906431c2f7a2152998e
Kamil Kojs <kamil.kojs@gmail.com>
2020-05-26 16:17:39 +0200

    final project

---
+++ cw12/cw12/Data/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using cw12.Models;
+
+namespace MvcMovie.Data {
+
+    public class MvcMovieContext :DbContext {
+
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options) {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
--- cw12/cw12/Startup.cs
+++ cw12/cw12/Startup.cs
@@ -5,21 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
 
 namespace cw12 {
     public class Startup {
-        public Startup(IConfiguration configuration) {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env) {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllersWithViews();
+            services.AddDbContext<MvcMovieContext>(options =>
+            {
+                var connectionString = Configuration.GetConnectionString("MvcMovieContext");
+
+                if (Environment.IsDevelopment()) {
+                    options.UseSqlite(connectionString);
+                } else {
+                    options.UseSqlServer(connectionString);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
